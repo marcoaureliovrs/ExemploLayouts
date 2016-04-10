ec4491ff10773bd37748a8d9be89276d3442c7af
Marco Gorak <marco.gorak@gmail.com>
2016-04-10 10:29:48 -0300

    first commit

---
+++ app/src/main/java/br/com/marcogorak/exemplolayouts/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.marcogorak.exemplolayouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ex_card);
+    }
+}
